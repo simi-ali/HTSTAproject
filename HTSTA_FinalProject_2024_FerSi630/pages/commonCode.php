8edf063ce0d7d90adb9fd78161916a7548b1becf
fersi630 <fersi630@school.lu>
2025-10-24 14:24:42 +0200

    changses

--- HTSTA_FinalProject_2024_FerSi630/pages/commonCode.php
+++ HTSTA_FinalProject_2024_FerSi630/pages/commonCode.php
@@ -1,11 +1,30 @@
 <?php
+$language = "EN";
+
+if (isset($_GET["lang"])) {
+	$language = $_GET["lang"];
+}
+
+print("The current language is " . $language);
+
+$arrayOfTranslations = [];
+
+$fileTranslations = fopen("Translation.csv", "r");
+while (!feof($fileTranslations)) {
+	$lineFromeFile = fgets($fileTranslations);
+	$piecesOfTranslations = explode(";", $lineFromFile);
+	$arrayOfTranslations[$piecesOfTranslations[0]] = ($language == "EN") ? $piecesOfTranslations[1] : $piecesOfTranslations[2];
+}
+
 function navBar($currentPage)
 {
+	global $language;
+	global$arrayOfTranslations;
 	$pages = [
-		"Home" => "index.php",
-		"Country1" => "country1.php",
-		"Country2" => "country2.php",
-		"Register" => "register.php",
+		$arrayOfTranslations["HomeBtn"] => "index.php",
+		$arrayOfTranslations["Country1Btn"] => "country1.php",
+		$arrayOfTranslations["Country2Btn"] => "country2.php",
+		$arrayOfTranslations["RegisterBtn"] => "register.php",
 	];
 ?>
 	<header>
@@ -18,6 +37,16 @@ function navBar($currentPage)
 					</li>
 				<?php endforeach; ?>
 			</ul>
+
+			<form>
+				<select name="lang" onchange="this.form.submit()">
+					<option value="EN" <?php if ($language == "EN") print("selected"); ?>>English</option>
+					<option value="PT" <?php if ($language == "PT") print("selected"); ?>>Portuguese</option>
+				</select>
+			</form>
+
+
+
 		</nav>
 	</header>
 <?php
@@ -25,22 +54,23 @@ function navBar($currentPage)
 ?>
 
 <?php
-	function userAlreadyRegistered($checkedUser){
-		//this function checks if $checkUser string is an existing user in the Clients.csv
-		//IF the given user IS already in the file we will return TRUE -> user already registered
-		//IF NOT (the user did not register) we will return from this function FALSE 
+function userAlreadyRegistered($checkedUser)
+{
+	//this function checks if $checkUser string is an existing user in the Clients.csv
+	//IF the given user IS already in the file we will return TRUE -> user already registered
+	//IF NOT (the user did not register) we will return from this function FALSE 
 
-		$bReturnValue = false; //the user is NOT in our database
+	$bReturnValue = false; //the user is NOT in our database
 
-		$fHandler = fopen("Clients.csv", "r");
-		while(!feof($fHandler)){
-			$newLine = fgets($fHandler);
-			$items = explode(";",$newLine);
-			if ($items[0] == $checkedUser){
-				$bReturnValue = true;
-			}
+	$fHandler = fopen("Clients.csv", "r");
+	while (!feof($fHandler)) {
+		$newLine = fgets($fHandler);
+		$items = explode(";", $newLine);
+		if ($items[0] == $checkedUser) {
+			$bReturnValue = true;
 		}
-
-		return $bReturnValue;
 	}
+
+	return $bReturnValue;
+}
 ?>
